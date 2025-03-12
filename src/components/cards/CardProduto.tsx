2c4d20f8a238e3e30a8fee0938f37172b0c3e455
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-12 11:41:08 -0300

    Melhorando o componente de CardProduto

--- src/components/cards/CardProduto.tsx
+++ src/components/cards/CardProduto.tsx
@@ -1,32 +1,56 @@
-import Image from "next/image";
-import { ReactNode } from "react";
+"use client"
+
+import { ReactNode, useState } from "react";
+import Image, { StaticImageData } from "next/image";
+import { FaRegHeart, FaHeart  } from 'react-icons/fa';
 
 interface CardProdutoProps {
-    imagem: string;
+    // imagem: string;
+    imagem: StaticImageData;
     altImagem: string;
     descricao: string;
-    children: ReactNode;
 }
 
-export const CardProduto = ({ imagem, altImagem, descricao, children }: CardProdutoProps) => {
+export const CardProduto = ({ imagem, altImagem, descricao }: CardProdutoProps) => {
+
+    const [favoritar, setFavoritar] = useState(false)
+
+    function addAosFavoritos() {
+        setFavoritar(!favoritar)
+
+        if (favoritar === false) {
+            alert("Adicionado aos favoritos")
+        }
+    }
+
     return (
-        <div>
-            <div>
-                {/* AQUI FICARÁ A IMAGEM */}
+        <div className="
+          w-[250px] h-[350px] p-5 border-2 flex-col items-center justify-evenly relative 
+        ">
+
+            <button className='absolute top-1 right-1' onClick={addAosFavoritos}>
+                {
+                    favoritar ? <FaHeart size={25} color='red' /> : <FaRegHeart size={25} color='red' />
+                }
+            </button>
+
+            <div className='mb-4'>
                 <Image
                     src={imagem}
                     alt={altImagem}
                 />
             </div>
 
-            <div>
-                <p>
-                    {descricao}
-                </p>
-                <div>
-                    <button>Adicionar ao Carrinho</button>
-                    <button>
-                        {children}
+            <div className='w-full flex flex-col items-center justify-evenly gap-4'>
+                <div className='w-full flex items-center justify-center'>
+                    <p className='text-[14px] italic text-center'>
+                        {descricao}
+                    </p>
+                </div>
+
+                <div className='w-full flex items-center justify-center'>
+                    <button className='border p-2 rounded-sm'>
+                        Adicionar ao carrinho
                     </button>
                 </div>
             </div>
