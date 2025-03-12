4ebd08979a820588a6dbabc94a2201a41156b86f
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-12 11:52:34 -0300

    Criando o componente de menu principal

---
+++ src/components/MenuPrincipal.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link"
+
+export const MenuPrincipal = () => {
+    return(
+        <header className="
+          w-full max-w-[1500px] h-[65px] m-auto pl-10 pr-10 flex items-center justify-between
+        ">
+          <div className="h-full flex items-center justify-center">
+            <a href="/">LOGO</a>
+          </div>
+
+          <nav className="w-[30%] h-full flex items-center justify-center border">
+            <ul className="w-full h-full flex items-center justify-evenly">
+              <Link href='/'>Icício</Link>
+              <Link href='/produtos'>Produtos</Link>
+              <Link href='/pedidos'>Pedidos</Link>
+              <Link href=''>Contatos</Link>
+            </ul>
+          </nav>
+        </header>
+    )
+}
