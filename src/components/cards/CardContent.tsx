a41db539569f54b618130d6667937cf58b8b9913
Bruno Santos <brunosdesenvolvedor@gmail.com>
2025-03-12 12:28:01 -0300

    Melhorando a logica do componente

--- src/components/cards/CardContent.tsx
+++ src/components/cards/CardContent.tsx
@@ -1,9 +1,24 @@
 import { ReactNode } from "react"
 
-export const CardContent = ({ children }: { children: ReactNode }) => {
+interface CardContentProps {
+    isDescription: boolean;
+    descricao?: string;
+}
+
+export const CardContent = ({ isDescription, descricao }: CardContentProps) => {
     return (
         <div className='w-full flex items-center justify-center'>
-            {children}
+
+            {isDescription ? (
+                <p className='text-[14px] italic text-center'>
+                    {descricao}
+                </p>
+            ) : (
+                <button className='border p-2 rounded-sm text-[14px] cursor-pointer'>
+                    Adicionar ao carrinho
+                </button>
+            )
+            }
         </div>
     )
 }
